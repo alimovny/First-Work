60136b34a9dea4ffd588561f502bf590325f28ce
alimovny <alimovny04@gmail.com>
2024-04-02 16:14:41 +0500

    Entered

---
+++ src/uz/pdp/Main.java
@@ -0,0 +1,14 @@
+package uz.pdp;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String str = scanner.nextLine();
+		
+		System.out.println("Your name -> " + str);
+		
+	}
+}
