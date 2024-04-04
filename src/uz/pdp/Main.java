f3fbdd403c712ceef0ec99d787f4c40e6e0db7c5
alimovny <alimovny04@gmail.com>
2024-04-04 13:32:01 +0500

    Changed

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -1,16 +1,22 @@
 package uz.pdp;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter your name: ");
-		String str = scanner.nextLine();
 		
-		for (int i = 0; i <= 16; i++) {
-			System.out.println("Your name -> " + str);
-		}
-		
+				Scanner input = new Scanner(System.in);
+				
+				System.out.print("Enter your birthday (YYYY-MM-DD): ");
+				String birthdayString = input.nextLine();
+				LocalDate birthday = LocalDate.parse(birthdayString);
+				
+				LocalDate today = LocalDate.now();
+				
+				long daysSinceBirthday = ChronoUnit.DAYS.between(birthday, today);
+				
+				System.out.println("Days since your birthday: " + daysSinceBirthday);
 	}
 }
